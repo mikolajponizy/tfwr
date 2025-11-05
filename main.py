6b745f74c431145d3aaff31b3eee623bf03b556a
mikolajponizy <mikolaj213@gmail.com>
2025-11-05 19:21:03 +0100

    Added .gitignore to ignore save file

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-def is even(n):
-	return n % 2 == 0 
+def drzewo(x,y):
+	if x % 2 == 0:
+		if y % 2 != 0:
+			return True
+		else:
+			return False
+	else:
+		return False
+
+
+size = get_world_size()
 
 while True:
-	for x in range(get_world_size()):
-		for y in range(get_world_size()):
-			
-	
+	for x in range(size):
+		for y in range(size):
+			if drzewo(x,y):
+				plant(Entities.Tree)
+			move(East)
+		move(North)
+		
