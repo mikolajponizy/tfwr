2ba4d2cfb66aac8ceb09456b1a4794d255dc02db
mikolajponizy <mikolaj213@gmail.com>
2025-11-05 20:05:13 +0100

    This can produce trees, carrots and grass waters them

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ def drzewo(x,y):
 			return False
 	else:
 		return False
+	
+def marchew(x,y):
+	if x % 2 == 0:
+		if y % 2 != 0:
+			return False
+		else:
+			return True
+	else:
+		return False
+	
+def podlewanie(x,y):
+	if get_water() < 0.25:
+		use_item(Items.Water)
 
 
 size = get_world_size()
@@ -14,7 +27,26 @@ while True:
 	for x in range(size):
 		for y in range(size):
 			if drzewo(x,y):
+				if can_harvest():
+					harvest()
 				plant(Entities.Tree)
+				podlewanie(x,y)
+			elif marchew(x,y):
+				if get_ground_type() != Grounds.Soil:
+					if can_harvest():
+						harvest()
+					till()
+					plant(Entities.Carrot)
+					podlewanie(x,y)
+				else:
+					if can_harvest():
+						harvest()
+					plant(Entities.Carrot)
+					podlewanie(x,y)
+			else:
+				if can_harvest():
+					harvest()
+				plant(Entities.Grass)
+				podlewanie(x,y)
 			move(East)
 		move(North)
-		
